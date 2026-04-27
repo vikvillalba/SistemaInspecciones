6294da925406ec8b38c04b04befff70a4543e25f
pablozg12 <pablozg120@gmail.com>
2026-04-26 22:52:26 -0700

    QA y ERP

---
+++ ServicioQA/servicioQA.py
@@ -0,0 +1,22 @@
+import asyncio
+import websockets
+import json
+
+async def escuchar_notificaciones_qa():
+    uri = "ws://localhost:8000/ws/qa" 
+    async with websockets.connect(uri) as websocket:
+        print("[QA] Conectado")
+        try:
+            while True:
+                mensaje = await websocket.recv()
+                datos = json.loads(mensaje)
+                
+                # Si es crítico, lanzar alerta
+                if datos.get("requiere_atencion"):
+                    print(f"ALERTA CRÍTICA: Defecto {datos['codigo_defecto']} en producto {datos['sku_producto']}")
+                else:
+                    print(f"Notificación recibida: {datos['nombre_defecto']}")
+        except websockets.ConnectionClosed:
+            print("Conexión cerrada con el servidor")
+
+asyncio.run(escuchar_notificaciones_qa())
