8c9aa224561d33c8dd1f8308328466568585d726
Marisol <marisol.ruiz253120@potros.itson.edu.mx>
2026-05-20 00:05:37 -0700

    nginx question mark

--- CatalogoProductos/target/quarkus-grpc11429024569636758236.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Program Files\Apache NetBeans\jdk\bin\java.EXE" -cp "C:\Users\erika\.m2\repository\io\quarkus\quarkus-grpc-protoc-plugin\3.15.1\quarkus-grpc-protoc-plugin-3.15.1-shaded.jar" io.quarkus.grpc.protoc.plugin.MutinyGrpcGenerator
--- CatalogoProductos/target/quarkus-grpc6924739405395637378.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-"c:\Users\pablo\.vscode\extensions\redhat.java-1.54.0-win32-x64\jre\21.0.10-win32-x86_64\bin\java.EXE" -cp "C:\Users\pablo\.m2\repository\io\quarkus\quarkus-grpc-protoc-plugin\3.15.1\quarkus-grpc-protoc-plugin-3.15.1-shaded.jar" io.quarkus.grpc.protoc.plugin.MutinyGrpcGenerator
